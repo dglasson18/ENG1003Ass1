960f3872bf9371d652692cd9d910c89993832059
dglasson18 <47996639+dglasson18@users.noreply.github.com>
2019-04-18 22:43:19 +1000

    18/04 Update
    Removed unused functions, changed input file to input.txt, added functions for reading cipher from a file and writing string to a file from stdin

--- Assessment1.c
+++ Assessment1.c
@@ -7,34 +7,33 @@
 	- Decrypt a message encrypted using a rotation cipher given cipher text only
 	- Decrypt a message encrypted with a subtitution cipher given cipher text only
 */
-char str12[1000];
 
-char completeString[1000];
 /*Function Headers
 const char encryptRotation(char *input[]);
 */
-char * rotationEncryption(int cipher);
+void rotationEncryption(int cipher);
 char * substitutionEncryption();
 char * substitutionDecryption();
 char * rotationDecryption();
 char * statRotDecryption(int option);
 int mostCommonLetter();
 int rotationCipherCalculator(int option, int a);
-char *stringMaker();
+char *stringMakerOutput();
 int wordChecker(); //returns 1 if the decrypted cipher creates a common word such as 'and' or 'the'
+int readCipher();
+int writeCipher(int option);
+void plainTextWriter();
 
 int main()
 {
 	//User assigns value based on choice below.
 	int choice = 0;
-	char toDecrypt[50];
+	char toDecrypt[50], finalString[1000], secChoice = 0;
 	int cipher, counter = 0;
 	int end = 1;
 	int m;
 	char *string;
-	int c = mostCommonLetter();
-	printf("%d\n", c);
-	
+
 	/*Basic User Interface
 	Available functions are displayed from the user.
 	They enter a value corresponding to the function that they wish to use.
@@ -63,30 +62,99 @@ int main()
 	{
 	case 1:
 		//This should call the function that will be used to encrypt rotation cipher
-		printf("You selected 'Encrypt Rotation Cipher' Please enter the key: ");
-		scanf("%d", &cipher);
-		rotationEncryption(cipher);
+		printf("\nYou selected 'Encrypt Rotation Cipher' You can choose from the following options:\n");
+		printf("	(a) Read plain text from 'input.txt' and cipher key from 'cipher.txt'\n");
+		printf("	(b) Read plain text from 'input.txt' and cipher key from stdin\n");
+		printf("	(c) Read plain text from stdin and cipher key from 'cipher.txt'\n");
+		printf("	(d) Read plain text and cipher key from stdin\n");
+		printf("\nPlease enter a letter corresponding to your preference: ");
+		scanf("%c", &secChoice);
+		while (secChoice < 97 || secChoice > 100)
+		{
+			printf("Please enter a valid option\n");
+			scanf("%c", &secChoice);
+		}
+		switch(secChoice)
+		{
+			case 97:
+				rotationEncryption(readCipher());
+				break;
+			case 98:
+				cipher = writeCipher(1);
+				rotationEncryption(cipher);
+				break;
+			case 99:
+				plainTextWriter();
+				rotationEncryption(readCipher());
+				break;
+			case 100:
+				plainTextWriter();
+				cipher = writeCipher(1);
+				rotationEncryption(cipher);
+				break;
+		}
+		printf("\nThe resulting string is: '%s'", stringMakerOutput());
 		break;
 	case 2:
 		//This should call the function that will be used to decrypt rotation cipher
-		printf("You selected 'Decrypt Rotation Cipher' Please enter the key that was used: ");
+		printf("\nYou selected 'Decrypt Rotation Cipher' You can choose from the following options:\n");
+		printf("	(a) Read plain text from 'input.txt' and cipher key from 'cipher.txt'\n");
+		printf("	(b) Read plain text from 'input.txt' and cipher key from stdin\n");
+		printf("	(c) Read plain text from stdin and cipher key from 'cipher.txt'\n");
+		printf("	(d) Read plain text and cipher key from stdin\n");
+		printf("Please enter a letter corresponding to your preference:");
+		scanf("\n%c", &secChoice);
+		while (secChoice < 97 || secChoice > 100)
+		{
+			if (secChoice < 97 || secChoice > 100)
+			{
+				printf("\nPlease enter a valid option:");
+				scanf("\n%c", &secChoice);
+			}
+			else 
+				break;
+		}
+		switch(secChoice)
+		{
+			case 97:
+				rotationEncryption(readCipher());
+				break;
+			case 98:
+				cipher = writeCipher(2);
+				rotationEncryption(cipher);
+				break;
+			case 99:
+				plainTextWriter();
+				rotationEncryption(readCipher());
+				break;
+			case 100:
+				cipher = writeCipher(2);
+				plainTextWriter();
+				//cipher = writeCipher(2);
+				rotationEncryption(cipher);
+				break;
+		}
+		/*
 		scanf("%d", &cipher);
 		cipher = cipher * -1;
-		rotationEncryption(cipher);
+		rotationEncryption(cipher);*/
+		printf("\nThe resulting string is: '%s'", stringMakerOutput());
 		break;
 	case 3:
 		//This should call the function that will be used to encrypt substitution cipher
 		printf("Encrypt Substitution Cipher");
 		substitutionEncryption();
+		printf("\nThe resulting string is: '%s'", stringMakerOutput());
 		break;
 	case 4:
 		//This should call the function that will be used to decrypt substitution cipher
 		printf("Decrypt Substitution Cipher");
 		substitutionDecryption();
+		printf("\nThe resulting string is: '%s'", stringMakerOutput());
 		break;
 	case 5:
 		m = mostCommonLetter();
-		printf("%d\n", m);
+		//printf("%d\n", m);
 		//cipher = (-1) * rotationCipherCalculator(0, m);
 		//printf("%d\n", cipher);
 		//rotationEncryption(cipher);
@@ -96,12 +164,13 @@ int main()
 		while (result != 1 && counter < 25)
 		{
 			cipher = (-1) * rotationCipherCalculator(counter, m);
-			printf("Testing key %d\n", cipher * -1);
+			//printf("Testing key %d\n", cipher * -1);
 			rotationEncryption(cipher);
 			result = wordChecker();
 			counter = counter + 1;
 		}
-		printf("%s", str12);
+		//finalString = stringMakerOutput();
+		printf("\nThe resulting string is: '%s'", stringMakerOutput());
 		break;
 	default:
 		//This will happen if user doesn't select an option from 1 to 4 or something else goes wrong
@@ -118,56 +187,27 @@ const char encryptRotation(char *input[])
 }
 */
 	
-char * rotationEncryption(int cipher)
+void rotationEncryption(int cipher)
 {
-    /*char str[50]; 
-	int cipher;
-	printf("Please enter text to be encrypted:");
-    scanf("%s[^\n]", str);
-	printf("Please enter the cipher key:\n");
-	scanf("%d\n", &cipher);
-	*/
+
 	FILE *input;
 	FILE *output;
 	
-	input = fopen("test.txt", "r");
+	input = fopen("input.txt", "r");
 	output = fopen("output.txt", "w");
-	
-	/*char str[1000];
-	
-	fscanf(input, "%s", &str);
-	int n;
-	for (n = 0; str[n] != 0; n++);
-	{
-		if (str[n] > 96 && str[n] < 123)
-		{
-			str[n] = str[n] - 32;
-		}
-		if (str[n] > 64 && str [n] < 91)
-		{
-			str[n] = str[n] + cipher;
-			if (str[n] > 90)
-				str[n] = str[n] - 26;
-			else if (str[n] < 65)
-				str[n] = str[n] + 26;
-		}
-		printf("%s", str);
-		//fprintf(output, "%", str[n]);
-	}
-	fprintf(output, "%s", str[n]);
-	fclose(output);
-	fclose(input);
-}
-*/
-	int c;
+
+	int c, end = 0;
 	char str;
-	
-	while (1)
+	//printf("%d\n", cipher);
+	while (end != 1)
 	{
 		str = fgetc(input);
 		
 		if(str == EOF)
+		{
+			end = 1;
 			break;
+		}
 		else
 		{
 			if (str > 96 && str < 123)
@@ -189,43 +229,6 @@ char * rotationEncryption(int cipher)
 	fclose(input);
 	fclose(output);
 }
-/*				
-		//if (feof(input) != 0)
-			//break;
-		
-		fscanf(input, "%c", &str);
-
-		if (str == 32 || str == '?' || str == '.' || str == ':' || str == '\'')
-		{	
-			;
-		}			
-
-		else
-		{
-			if (str>64 && str <91)
-				str = str + 32;
-			str = str + cipher;
-			if (str >= 123 || str < 0)
-			{
-				if (str >= 123)
-				{
-					str = str-26;
-				}
-				else if (str < 0)
-				{
-					str = -1 * str -18;
-				}
-			}
-			else if (str <= 96)
-			{
-				str = str + 26;
-			}
-		}
-		fprintf(output, "%c", str);
-	}
-	fclose(output);
-}
-*/
 
 char * substitutionEncryption()
 {
@@ -342,41 +345,6 @@ char * substitutionEncryption()
 	fclose(keyText);
 }
 
-char * rotationDecyption()
-{    
-    char str[50]; 
-	int cipher = -1;
-    scanf("%[^'\n']s", str);
-	
-	for (int n = 0; str[n] != 0; n++)
-		if (str[n] == 32 || str[n] == 58 || str[n] == 46 || str[n] == 44)
-		{	
-			str[n] = str[n];
-		}
-		else
-		{
-			str[n] = str[n] - cipher;
-			if (str[n] >= 123 || str[n] < 0)
-			{
-				if (str[n] >= 123)
-				{
-					str[n] = str[n]-26;
-				}
-				else if (str[n] < 0)
-				{
-					str[n] = -1 * str[n] -18;
-				}
-			}
-			else if (str[n] <= 96)
-			{
-				str[n] = str[n] + 26;
-			}
-		}
-    printf("%s", str); 
-  
-    return 0; 
-}
-
 char * substitutionDecryption()
 {
 	char key[100]; //= {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','\0'};
@@ -424,203 +392,13 @@ char * substitutionDecryption()
 	fclose(keyText);
 }
 
-char * statRotDecryption(int option)
-{
-	FILE *input;
-	FILE *output;
-	char str[1000];
-	int n = 0, cipher;
-	
-	input = fopen("test.txt", "r");
-	output = fopen("output.txt", "r+");
-	
-	int strCount[27];
-	int a, fish = 10;
-	
-	while(feof(input) == 0)
-	{
-		fscanf(input, "%c", &str[n]);
-		if (str[n] > 64 && str[n] < 91)
-			str[n] = str[n] + 32;
-		n++;
-		
-	}
-	for (int n = 0; n<27; n++)
-		strCount[n] = 0;
-	for (int n = 0; str[n] !=0; n++)
-	{
-		switch (str[n])
-		{
-			case 97:
-				strCount[0]++;
-				break;
-			case 98:
-				strCount[1]++;
-				break;
-			case 99:
-				strCount[2]++;
-				break;
-			case 100:
-				strCount[3]++;
-				break;
-			case 101:
-				strCount[4]++;
-				break;
-			case 102:
-				strCount[5]++;
-				break;
-			case 103:
-				strCount[6]++;
-				break;
-			case 104:
-				strCount[7]++;
-				break;
-			case 105:
-				strCount[8]++;
-				break;
-			case 106:
-				strCount[9]++;
-				break;
-			case 107:
-				strCount[10]++;
-				break;
-			case 108:
-				strCount[11]++;
-				break;
-			case 109:
-				strCount[12]++;
-				break;
-			case 110:
-				strCount[13]++;
-				break;
-			case 111:
-				strCount[14]++;
-				break;
-			case 112:
-				strCount[15]++;
-				break;
-			case 113:
-				strCount[16]++;
-				break;
-			case 114:
-				strCount[17]++;
-				break;
-			case 115:
-				strCount[18]++;
-				break;
-			case 116:
-				strCount[19]++;
-				break;
-			case 117:
-				strCount[20]++;
-				break;
-			case 118:
-				strCount[21]++;
-				break;
-			case 119:
-				strCount[22]++;
-				break;
-			case 120:
-				strCount[23]++;
-				break;
-			case 121:
-				strCount[24]++;
-				break;
-			case 122:
-				strCount[25]++;
-				break;
-			default:
-				break;
-		}
-	}
-	for (int n = 0; n<27; n++)
-	{
-		static int count = 0;
-		if ((int)strCount[n] > count)
-		{
-			count = strCount[n];
-			a = n; //holds the value in the string that corresponds to the letter in the alpahbet
-			//i.e. a=0, b=1 etc.
-	    }
-	}
-	if (option == 0)
-	{
-		cipher = a + 97 - 101;
-		printf("Cipher 0 Working");
-	}
-	else if (option ==1)
-	{
-		cipher = a + 97 - 116;
-		printf("Cipher 1 Working");
-	}
-	else if (option == 2)
-	{
-		cipher = a;
-		printf("Cipher 2 Working");
-	}
-	else if (option == 3)
-	{
-		cipher = a + 97 - 111;
-		printf("Cipher 3 Working");
-	}
-	
-		
-	if (cipher <= 0 )
-	{
-		cipher = cipher + 26;
-		printf("Cipher adjusted");
-	}	
-	
-	char strCopy[1000];
-	for (int n = 0; str[n] != 0; n++)
-	{
-		strCopy[n] = str[n];
-}		
-	for (;fish !=0;)
-	{
-		printf("Entered initial for loop");
-		for (int n = 0; str[n] != 0; n++)
-		{
-			
-			
-			if (strCopy[n] == 32 || strCopy[n] == 58 || strCopy[n] == 46 || strCopy[n] == 44 || strCopy[n] == 47)
-			{	
-				strCopy[n] = strCopy[n];
-			}
-			else
-			{
-				strCopy[n] = strCopy[n] - cipher;
-				if (strCopy[n] >= 123 || strCopy[n] < 0)
-				{
-					if (strCopy[n] >= 123)
-					{
-						strCopy[n] = str[n]-26;
-					}
-					else if (strCopy[n] < 0)
-					{
-						strCopy[n] = -1 * str[n] -18;
-					}
-				}
-				else if (strCopy[n] <= 96)
-				{
-					strCopy[n] = strCopy[n] + 26;
-				}
-			}
-		}
-		fish = 0;
-		printf("%s", strCopy);
-	}
-	fprintf(output, "%s", strCopy);
-    return str; 
-}
-
 int mostCommonLetter()
 {
 	FILE *input;
-	char str[1000];
+	char str[10000];
 	int n = 0, a;
 	
-	input = fopen("test.txt", "r");
+	input = fopen("input.txt", "r");
 		
 	int strCount[27];
 	
@@ -633,11 +411,12 @@ int mostCommonLetter()
 			str[n] = str[n] - 32;
 		n++;
 	}
-	for (int n = 0; n<27; n++)
+	//printf("%s\n", str);
+	for (n = 0; n<27; n++)
 	{
 		strCount[n] = 0;
 	}
-	for (int n = 0; str[n] !=0; n++)
+	for (n = 0; str[n] != 0; n++)
 	{
 		switch (str[n])
 		{
@@ -724,7 +503,7 @@ int mostCommonLetter()
 		}
 	}
 	
-	for (int n = 0; n<27; n++)
+	for (n = 0; n<27; n++)
 	{
 		static int count = 0;
 		if (strCount[n] > count)
@@ -733,19 +512,18 @@ int mostCommonLetter()
 			a = n; //holds the value in the string that corresponds to the letter in the alpahbet
 			//i.e. a=0, b=1,... z=26
 	    }
-		printf("a = %d\n", a);
 	}
+	printf("%d\n", a);
 	return a;
 }
 
 int rotationCipherCalculator(int option, int a)
 {
-	int static cipher;
+	int static cipher = 0;
 	switch (option)
 	{
 		case 0:
 			cipher = a + 97 - 101;
-			printf("Cipher 0 Working");
 			break;
 		case 1:
 			cipher  = 26 + 97 - 101;
@@ -774,12 +552,138 @@ int rotationCipherCalculator(int option, int a)
 		case 9:
 			cipher = 26 + 97 - 111;
 			break;
+		case 10:
+			cipher = a + 97 - 110;
+			break;
+		case 11:
+			cipher = 26 + 97 -110;
+			break;
+		case 12:
+			cipher = a + 97 - 115;
+			break;
+		case 13:
+			cipher = 26 + 97 - 115;
+			break;
+		case 14:
+			cipher = a + 97 - 108;
+			break;
+		case 15:
+			cipher = 26 + 97 - 115;
+			break;
+		case 16:
+			cipher = a + 97 - 108;
+			break;
+		case 17:
+			cipher = 26 + 97 - 108;
+			break;
+		case 18:
+			cipher = a + 97 - 99;
+			break;
+		case 19:
+			cipher = 26 + 97 - 99;
+			break;
+		case 20:
+			cipher = a + 97 - 117;
+			break;
+		case 21:
+			cipher = 26 + 97 - 117;
+			break;
+		case 22:
+			cipher = a + 97 - 100;
+			break;
+		case 23:
+			cipher = 26 + 97 - 100;
+			break;
+		case 24:
+			cipher = a + 97 - 112;
+			break;
+		case 25:
+			cipher = 26 + 97 - 112;
+			break;
+		case 26:
+			cipher = a + 97 - 109;
+			break;
+		case 27:
+			cipher = 26 + 97 - 109;
+			break;
+		case 28:
+			cipher = a + 97 - 104;
+			break;
+		case 29:
+			cipher = 26 + 97 - 104;
+			break;
+		case 30:
+			cipher = a + 97 - 103;
+			break;
+		case 31:
+			cipher = 26 + 97 - 103;
+			break;
+		case 32:
+			cipher = a + 97 - 98;
+			break;
+		case 33:
+			cipher = 26 + 97 - 98;
+			break;
+		case 34:
+			cipher = a + 97 - 102;
+			break;
+		case 35:
+			cipher = 26 + 97 - 102;
+			break;
+		case 36:
+			cipher = a + 97 - 121;
+			break;
+		case 37:
+			cipher = 26 + 97 - 121;
+			break;
+		case 38:
+			cipher = a + 97 - 119;
+			break;
+		case 39:
+			cipher = 26 + 97 - 119;
+			break;
+		case 40:
+			cipher = a + 97 - 107;
+			break;
+		case 41:
+			cipher = 26 + 97 - 107;
+			break;
+		case 42:
+			cipher = a + 97 - 118;
+			break;
+		case 43:
+			cipher = 26 + 97 - 118;
+			break;
+		case 44:
+			cipher = a + 97 - 120;
+			break;
+		case 45:
+			cipher = 26 + 97 - 120;
+			break;
+		case 46:
+			cipher = a + 97 - 122;
+			break;
+		case 47:
+			cipher = 26 + 97 - 122;
+			break;
+		case 48:
+			cipher = a + 97 - 106;
+			break;
+		case 49:
+			cipher = 26 + 97 - 106;
+			break;
+		case 50:
+			cipher = a + 97 - 113;
+			break;
+		case 51:
+			cipher = 26 + 97 - 113;
+			break;
 		default:
-			cipher++;
+			break;
 	}
 	return cipher;
 }
-char *stringMaker()
+char *stringMakerOutput()
 {
 	FILE *input;
 	char fileName[100];
@@ -805,16 +709,55 @@ char *stringMaker()
 	//printf("String Tested is %s\n", str);
 	return str;
 }
+
+int readCipher()
+{
+	FILE *input;
+	char c;
+	int cipher;
+	input = fopen("cipher.txt", "r");
+	c = fgetc(input);
+	cipher = c - '0';
+	fclose(input);
+	return cipher;
+}
+
+int writeCipher(int option)
+{
+	int cipher;
+	if (option == 1)
+	{
+		printf("Please enter the value for the cipher, between 1 and 26 (If you want to you can go between -26 and 26 and everything should still work)\n");
+	}
+	else
+	{
+		printf("Please enter the value of the cipher that was used to encrypt the text originally (between -26 and 26)\n");
+	}
+	scanf("%d", &cipher);
+	
+	while (cipher < -26 || cipher > 26)
+	{
+		printf("\nThe value that you've entered is invalid, please try again: ");
+		scanf("%d", &cipher);
+	}
+	
+	if (option != 1)
+	{
+		cipher = cipher * -1;
+	}
+	
+	return cipher;
+}
 	
 //10 Most common words - https://simple.wikipedia.org/wiki/Most_common_words_in_English
 int wordChecker()
 {
 	char * stringToBeChecked;
 	int complete = 0;
-	stringToBeChecked = stringMaker();
+	stringToBeChecked = stringMakerOutput();
 	for (int n = 0; stringToBeChecked[n] != 0; n++)
 	{
-	if ((stringToBeChecked[n] == 'A' && stringToBeChecked[n+1] == 'N' && stringToBeChecked[n+2] == 'D') ||(stringToBeChecked[n] == 'T' && stringToBeChecked [n+1] == 'H' && stringToBeChecked[n+2] == 'E') || (stringToBeChecked[n] == ' ' && stringToBeChecked[n+1] == 'I' && stringToBeChecked[n+2] == 'T' && stringToBeChecked[n+3] == ' '))// && stringToBeChecked[n+1] == 'n' && stringToBeChecked[n+2] == 'd')
+		if ((stringToBeChecked[n] == 'A' && stringToBeChecked[n+1] == 'N' && stringToBeChecked[n+2] == 'D') ||(stringToBeChecked[n] == 'T' && stringToBeChecked [n+1] == 'H' && stringToBeChecked[n+2] == 'E') || (stringToBeChecked[n] == ' ' && stringToBeChecked[n+1] == 'I' && stringToBeChecked[n+2] == 'T' && stringToBeChecked[n+3] == ' '))// && stringToBeChecked[n+1] == 'n' && stringToBeChecked[n+2] == 'd')
 		{
 			complete = 1;
 			break;
@@ -825,13 +768,35 @@ int wordChecker()
 			stringToBeChecked[n+1] = NULL;
 		}
 	}
-	printf("String checked %s", stringToBeChecked);
+	//printf("String checked %s", stringToBeChecked);
 	if (complete == 1)
 		return 1;
 	else 
 		return 0;
 }
 
+void plainTextWriter()
+{
+	FILE * plainText = fopen("input.txt", "w");
+	char inputText[1000];
+	
+	printf("Please enter the plain text you would like to encrypt\n");
+	scanf("\n");
+	scanf("%[^\n]s", inputText);
+	printf("The string you entered is %s\n", inputText);
+	
+	for (int n = 0; inputText[n] != 0; n++)
+	{
+		fputc(inputText[n], plainText);
+	}
+	//fprintf(plainText, "%s", inputText);
+	
+	fclose(plainText);
+	
+	return;
+}
+	
+/*
 int subAnalysis()
 {
 	char encryptedString[1000];
@@ -842,5 +807,5 @@ int subAnalysis()
 		if (encryptedString == FEOF)
 			break;
 		else
-			
+*/		
 
